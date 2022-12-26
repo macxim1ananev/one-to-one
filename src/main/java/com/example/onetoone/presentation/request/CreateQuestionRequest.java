2902473c5a0ae7ec97c6b90f0824795626c9a2f3
Maksim Ananev <maksim.ananev.1994@mail.ru>
2022-12-26 21:56:43 +0300

    added validation for request

--- src/main/java/com/example/onetoone/presentation/request/CreateQuestionRequest.java
+++ src/main/java/com/example/onetoone/presentation/request/CreateQuestionRequest.java
@@ -3,9 +3,13 @@ package com.example.onetoone.presentation.request;
 import com.example.onetoone.core.question.commands.QuestionRequest;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
-public class CreateQuestionRequest2 {
+public class CreateQuestionRequest {
+    @NotNull
+    @NotBlank
     private List<QuestionRequest> questions;
 }
