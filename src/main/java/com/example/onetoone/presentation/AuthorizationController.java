08f714d2a9fc1c2ff9fb26bcd5b91c08425fc0e3
maxim <maksim.ananev.1994@mail.ru>
2023-02-07 15:51:31 +0500

    hot fix cors politic

--- src/main/java/com/example/onetoone/presentation/AuthorizationController.java
+++ src/main/java/com/example/onetoone/presentation/AuthorizationController.java
@@ -6,13 +6,11 @@ import com.example.onetoone.presentation.request.UserAuthorizationRequest;
 import com.example.onetoone.presentation.view.JwtTokenView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/auth")
 @RequiredArgsConstructor
 public class AuthorizationController {
