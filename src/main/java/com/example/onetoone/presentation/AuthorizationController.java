4f0071c5fac402964405df5e9e07b9854c6c9eb0
maxim <maksim.ananev.1994@mail.ru>
2023-02-07 17:57:36 +0500

    disable spring security

--- src/main/java/com/example/onetoone/presentation/AuthorizationController.java
+++ src/main/java/com/example/onetoone/presentation/AuthorizationController.java
@@ -1,33 +1,33 @@
-package com.example.onetoone.presentation;
-
-import com.example.onetoone.config.security.JwtTokenService;
-import com.example.onetoone.presentation.request.RefreshJwtTokenRequest;
-import com.example.onetoone.presentation.request.UserAuthorizationRequest;
-import com.example.onetoone.presentation.view.JwtTokenView;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-@Slf4j
-@RestController
-@CrossOrigin("http://localhost:3000")
-@RequestMapping("/v1/auth")
-@RequiredArgsConstructor
-public class AuthorizationController {
-    private final JwtTokenService jwtTokenService;
-
-
-    @PostMapping("/jwt")
-    public JwtTokenView oneTimeAuthorization(@RequestBody UserAuthorizationRequest request) {
-        log.info("Request to receive a jwt token");
-
-        return jwtTokenService.authenticate(request);
-    }
-
-    @PostMapping("jwt/refresh")
-    public JwtTokenView refreshJwtToken(@RequestBody RefreshJwtTokenRequest request) {
-        log.info("Request to update a jwt token");
-
-        return jwtTokenService.refreshAccessToken(request);
-    }
-}
+//package com.example.onetoone.presentation;
+//
+//import com.example.onetoone.config.security.JwtTokenService;
+//import com.example.onetoone.presentation.request.RefreshJwtTokenRequest;
+//import com.example.onetoone.presentation.request.UserAuthorizationRequest;
+//import com.example.onetoone.presentation.view.JwtTokenView;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Slf4j
+//@RestController
+//@CrossOrigin("http://localhost:3000")
+//@RequestMapping("/v1/auth")
+//@RequiredArgsConstructor
+//public class AuthorizationController {
+//    private final JwtTokenService jwtTokenService;
+//
+//
+//    @PostMapping("/jwt")
+//    public JwtTokenView oneTimeAuthorization(@RequestBody UserAuthorizationRequest request) {
+//        log.info("Request to receive a jwt token");
+//
+//        return jwtTokenService.authenticate(request);
+//    }
+//
+//    @PostMapping("jwt/refresh")
+//    public JwtTokenView refreshJwtToken(@RequestBody RefreshJwtTokenRequest request) {
+//        log.info("Request to update a jwt token");
+//
+//        return jwtTokenService.refreshAccessToken(request);
+//    }
+//}
