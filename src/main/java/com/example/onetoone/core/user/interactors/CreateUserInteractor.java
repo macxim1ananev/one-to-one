78fce7732e3b8c1e6e572f86b2cdcede7c77b709
maxim <maksim.ananev.1994@mail.ru>
2023-02-16 22:15:08 +0500

    fix bug

--- src/main/java/com/example/onetoone/core/user/interactors/CreateUserInteractor.java
+++ src/main/java/com/example/onetoone/core/user/interactors/CreateUserInteractor.java
@@ -1,14 +1,17 @@
 package com.example.onetoone.core.user.interactors;
 
 import com.example.onetoone.core.service.common.Interactor;
+import com.example.onetoone.core.service.error.ServiceException;
 import com.example.onetoone.core.service.interfaces.Users;
 import com.example.onetoone.core.user.UserMapper;
 import com.example.onetoone.core.user.commands.CreateUserCommand;
 import com.example.onetoone.core.user.results.UserResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.postgresql.shaded.com.ongres.scram.common.exception.ScramServerErrorException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -16,12 +19,17 @@ public class CreateUserInteractor implements Interactor<CreateUserCommand, UserR
     private final Users users;
     private final UserMapper mapper;
     private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserResult execute(CreateUserCommand command) {
         log.info("Executing command {}", command);
-
-        var entity = mapper.toEntity(command);
-        entity.setPassword(passwordEncoder.encode(command.getPassword()));
-        return mapper.toResult(users.put(entity));
+        var user = users.loadUserByEmail(command.getEmail());
+        if (user.isPresent()) {
+            throw new ServiceException(ServiceException.Exception.USER_BY_EMAIL_ALREADY_REGISTERED, command.getEmail());
+        } else {
+            var entity = mapper.toEntity(command);
+            entity.setPassword(passwordEncoder.encode(command.getPassword()));
+            return mapper.toResult(users.put(entity));
+        }
     }
 }
--- src/main/java/com/example/onetoone/presentation/AuthorizationController.java
+++ src/main/java/com/example/onetoone/presentation/AuthorizationController.java
@@ -1,7 +1,6 @@
 package com.example.onetoone.presentation;
 
 import com.example.onetoone.config.security.JwtTokenService;
-import com.example.onetoone.presentation.request.RefreshJwtTokenRequest;
 import com.example.onetoone.presentation.request.UserAuthorizationRequest;
 import com.example.onetoone.presentation.view.JwtTokenView;
 import lombok.RequiredArgsConstructor;
