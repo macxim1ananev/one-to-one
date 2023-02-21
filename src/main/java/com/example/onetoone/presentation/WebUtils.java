29a50f988ac14fdbbdd578db27c98d18f3932aaf
maxim <maksim.ananev.1994@mail.ru>
2023-02-21 10:04:48 +0500

    update criterua builder

--- src/main/java/com/example/onetoone/presentation/WebUtils.java
+++ src/main/java/com/example/onetoone/presentation/WebUtils.java
@@ -1,7 +1,10 @@
 package com.example.onetoone.presentation;
 
 import com.example.onetoone.core.service.common.SearchCriteria;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
@@ -10,22 +13,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebUtils {
-
+    private static final String STRING_PATTERN = "([\\w\\-.]?|([а-яА-ЯёЁ0-9_\\-.]?))+?";
     private static final String LOCAL_DATE_TIME_PATTERN = "(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})";
-    private static final List<String> OR_OPERATION_LIST = List.of(":", "!:");
+    private static final String LOCAL_DATE_PATTERN = "(\\d{4})-(\\d{2})-(\\d{2})";
+    private static final String EMAIL_PATTERN = "(?:[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[\\w]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
     public static Set<SearchCriteria> getCriteria(String search) {
-        var pattern = Pattern.compile("(\\w+?\\.?\\w+?)(:|>|>:|<|<:)((\\w+?(@(\\w+?\\.?)?)?\\w*)|" +
-                LOCAL_DATE_TIME_PATTERN + "),");
+        var pattern = Pattern.compile("(\\w+?\\.?\\w+?)(:|>|>:|<|<:|!:)("
+                + STRING_PATTERN + "|"
+                + EMAIL_PATTERN + "|"
+                + LOCAL_DATE_PATTERN + "|"
+                + LOCAL_DATE_TIME_PATTERN + "),");
         var matcher = pattern.matcher(search + ",");
         var criteria = new HashSet<SearchCriteria>();
         while (matcher.find()) {
-            var operation = matcher.group(2);
             criteria.add(SearchCriteria.builder()
                     .key(matcher.group(1))
                     .operation(matcher.group(2))
                     .value(getValue(matcher.group(3)))
-                    .isOrOperation(isOrOperation(operation) && countMatches(search, matcher.group(1)) > 1)
                     .build());
         }
         return criteria;
@@ -33,23 +38,20 @@ public class WebUtils {
 
     private static Object getValue(String group) {
         var localDateTimePattern = Pattern.compile(LOCAL_DATE_TIME_PATTERN);
+        var localDatePattern = Pattern.compile(LOCAL_DATE_PATTERN);
         if (localDateTimePattern.matcher(group).matches())
             return LocalDateTime.parse(group);
+        if (localDatePattern.matcher(group).matches())
+            return LocalDate.parse(group);
         return group;
     }
 
-    private static int countMatches(String search, String field) {
-        Pattern pattern = Pattern.compile(field + "(:|>|>:|<|<:)");
-        Matcher matcher = pattern.matcher(search);
-        int count = 0;
-        while (matcher.find()) {
-            count++;
-        }
-
-        return count;
-    }
-
-    private static boolean isOrOperation(String operation) {
-        return OR_OPERATION_LIST.contains(operation);
+    public static HttpHeaders getHeaders(String name) {
+        var contentDisposition = ContentDisposition.builder("inline")
+                .filename(name)
+                .build();
+        var headers = new HttpHeaders();
+        headers.setContentDisposition(contentDisposition);
+        return headers;
     }
 }
