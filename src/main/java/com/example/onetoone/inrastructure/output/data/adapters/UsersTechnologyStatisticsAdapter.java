1c979b36ed1a7519230414b8a6bcfa4613ee8bf6
maxim <maksim.ananev.1994@mail.ru>
2023-02-27 23:24:46 +0500

    add logging

--- src/main/java/com/example/onetoone/inrastructure/output/data/adapters/UsersTechnologyStatisticsAdapter.java
+++ src/main/java/com/example/onetoone/inrastructure/output/data/adapters/UsersTechnologyStatisticsAdapter.java
@@ -5,11 +5,12 @@ import com.example.onetoone.core.service.interfaces.UsersTechnologyStatistics;
 import com.example.onetoone.inrastructure.output.data.mappers.FullUsersStatisticsMapper;
 import com.example.onetoone.inrastructure.output.data.repositories.UsersTechnologyStatisticsRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UsersTechnologyStatisticsAdapter implements UsersTechnologyStatistics {
@@ -18,6 +19,7 @@ public class UsersTechnologyStatisticsAdapter implements UsersTechnologyStatisti
 
     @Override
     public UserTechnologyStatistics save(UserTechnologyStatistics statistics) {
+        log.info("Request for save user technology statistics");
         var model = mapper.toModel(statistics);
         return mapper.toEntity(repository.save(model));
     }
