282b3a6b003d9be1302b21a0955daf20a204b538
maxim <maksim.ananev.1994@mail.ru>
2023-03-11 16:44:37 +0500

    added verification about feedback already has been written

--- src/main/java/com/example/onetoone/inrastructure/output/data/repositories/FeedbackRepository.java
+++ src/main/java/com/example/onetoone/inrastructure/output/data/repositories/FeedbackRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface FeedbackRepository extends JpaRepository<FeedbackModel, Long> {
     Optional<FeedbackModel> getByOneToOneIdAndRecipientId(Long oneToOneId, Long recipientId);
+    Optional<FeedbackModel> getByOneToOneIdAndAuthorId(long oneToOneId, long authorId);
 }
