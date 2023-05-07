9fd70033811edf9eeb7d2d8b6524ddb2d1d87316
maxim <maksim.ananev.1994@mail.ru>
2023-05-07 19:46:35 +0500

    delete deprecated method

--- src/main/java/com/example/onetoone/presentation/FeedbackController.java
+++ src/main/java/com/example/onetoone/presentation/FeedbackController.java
@@ -2,30 +2,20 @@ package com.example.onetoone.presentation;
 
 import com.example.onetoone.core.feedback.commands.CreateFeedbackCommand;
 import com.example.onetoone.core.feedback.commands.GetByOneToOneAndRecipientIdCommand;
-import com.example.onetoone.core.feedback.commands.statistics.GetFullUserStatisticsCommand;
-import com.example.onetoone.core.feedback.commands.statistics.GetUserStatisticsCommand;
-import com.example.onetoone.core.feedback.commands.statistics.GetUserTechnologyStatisticsCommand;
-import com.example.onetoone.core.feedback.results.statistics.FullUserStatisticsResult;
-import com.example.onetoone.core.feedback.results.statistics.UserTechnologyStatisticsResult;
 import com.example.onetoone.core.service.command_bus.CommandBus;
-import com.example.onetoone.core.service.common.ResultModelList;
 import com.example.onetoone.core.user.entities.Permissions;
-import com.example.onetoone.presentation.common.ListView;
 import com.example.onetoone.presentation.mapper.FeedbackViewMapper;
 import com.example.onetoone.presentation.mapper.FullUsersStatisticsViewMapper;
 import com.example.onetoone.presentation.mapper.UsersStatisticsViewMapper;
 import com.example.onetoone.presentation.request.CreateFeedbackRequest;
 import com.example.onetoone.presentation.view.FeedbackView;
-import com.example.onetoone.presentation.view.FullUserStatisticsView;
-import com.example.onetoone.presentation.view.UserStatisticsView;
-import com.example.onetoone.presentation.view.UserTechnologyStatisticsView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.stream.Collectors;
+
 @Slf4j
 @RestController
 @RequestMapping("/v1/user/one-to-one/feedback")
@@ -61,48 +51,5 @@ public class FeedbackController {
                 .oneToOneId(oneToOneId)
                 .recipientId(recipientId)
                 .build()));
-    }
-    @Deprecated
-    @GetMapping("/{userId}/statistics")
-    @PreAuthorize("@securityManager.hasPermission('" + Permissions.Fields.GET_USER_STATISTICS + "')")
-    public UserStatisticsView getUserStatistics(@PathVariable Long userId){
-        log.info("Request for get user statistics");
-
-        return statisticsViewMapper.toView(commandBus.execute(GetUserStatisticsCommand
-                .builder()
-                .id(userId)
-                .build()));
-    }
-    @Deprecated
-    @GetMapping("/{userId}/full-statistics")
-    @PreAuthorize("@securityManager.hasPermission('" + Permissions.Fields.GET_FULL_USER_STATISTICS + "')")
-    public ListView<FullUserStatisticsView> getFullUserStatistics(@PathVariable Long userId){
-        log.info("Request for get full user statistics");
-
-        ResultModelList<FullUserStatisticsResult> resultList = commandBus.execute(GetFullUserStatisticsCommand
-                .builder()
-                .id(userId)
-                .build());
-
-        return new ListView<>(resultList.getTotalItems(), resultList.getItems()
-                .stream()
-                .map(fullStatisticsViewMapper::toView)
-                .collect(Collectors.toList()));
-    }
-    @Deprecated
-    @GetMapping("/{userId}/technology-statistics")
-    @PreAuthorize("@securityManager.hasPermission('" + Permissions.Fields.GET_USER_TECHNOLOGY_STATISTICS + "')")
-    public ListView<UserTechnologyStatisticsView> getUserTechnologyStatistics(@PathVariable Long userId){
-        log.info("Request for get user technology statistics");
-
-        ResultModelList<UserTechnologyStatisticsResult> resultList = commandBus.execute(GetUserTechnologyStatisticsCommand
-                        .builder()
-                        .id(userId)
-                        .build());
-
-        return new ListView<>(resultList.getTotalItems(), resultList.getItems()
-                .stream()
-                .map(fullStatisticsViewMapper::toTechnologyStatisticsView)
-                .collect(Collectors.toList()));
     }
 }
