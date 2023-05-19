a80eabc1177b0a9641022bf7f59e1d548b382d40
circloph <circloph@gmail.com>
2023-05-19 11:22:58 +0600

    solving the N+1 problem

--- src/main/java/com/example/onetoone/inrastructure/output/data/repositories/UserAnswerRepository.java
+++ src/main/java/com/example/onetoone/inrastructure/output/data/repositories/UserAnswerRepository.java
@@ -1,10 +1,12 @@
 package com.example.onetoone.inrastructure.output.data.repositories;
 
 import com.example.onetoone.inrastructure.output.data.models.UserAnswerModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserAnswerRepository extends JpaRepository<UserAnswerModel, Long> {
+    @EntityGraph(attributePaths = {"question", "question.technology"})
     List<UserAnswerModel> getAllByFeedbackId(Long feedbackId);
 }
